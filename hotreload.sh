93e6064502758357d83b6bc1bf28cd058f544290
EyeBallPaul <maik.tappe82@googlemail.com>
2025-11-19 06:04:16 +0100

    hot reload

---
+++ hotreload.sh
@@ -0,0 +1,28 @@
+# sudo apt install inotify-tools
+# pacman -S mingw-w64-x86_64-fswatch
+
+
+
+while inotifywait -q -e close_write test.py; do
+    clear
+    echo "=== Starte neu: $(date) ==="
+    python3 test.py
+    echo
+    echo "=== Programm beendet, warte auf nächste Änderung... ==="
+done
+
+
+
+#### Mit fswatch ####
+#!/bin/bash
+
+#clear
+#echo "Watching test.py..."
+
+#fswatch -o test.py | while read num; do
+#    clear
+#    echo "=== Neustart um $(date) ==="
+#    python test.py
+#    echo
+#    echo "=== Warten auf nächste Änderung... ==="
+#done
